6e790222544f293d8a630a3e44e30d481d3fb8e3
ozf1r <155823834+ozf1r@users.noreply.github.com>
2024-02-02 19:51:18 +0200

    Create biblesearch.pl

---
+++ biblesearch.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+# biblesearch.pl - diatheke search made easy
+# 2.2.2024 Late@Linuxrauta.fi
+#
+# Public Domain
+#
+# Usage:
+#
+# biblesearch.pl search_string range module
+#
+# You need diatheke and the sword library installed
+#
+
+use strict;
+use warnings;
+
+my $search = $ARGV[0];
+
+my $range = $ARGV[1];
+
+my $module = $ARGV[2];
+
+my $total;
+
+print "Searching: " . $search . "\n";
+print "Range: " . $range . "\n";
+print "Module: " . $module . "\n";
+
+# Call diatheke and capture its output
+my $output = `diatheke -b $module -s phrase -r $range -k $search`;
+
+my @verses;
+
+while ($output =~ /((?:\w+\s+){1,4})(\d+:\d+)/g) {
+    my $verse_string = "$1 $2";
+    push @verses, $verse_string;
+}
+
+while ($output =~ /(\d+)\s+matches\s+total/g) {
+    $total = $1;
+}
+
+# Print the extracted Bible verses
+print "Bible Verses:\n";
+foreach my $verse (@verses) {
+
+    my $output = `diatheke -b $module -k $verse`;
+
+    print $output . "\n";
+}
+
+print $total . " verses found\n";
